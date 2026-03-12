9991b9884e39a563f2f5c47c655ca57aa6585814
elkshop009-dot <elkshop009@gmail.com>
2026-03-12 00:45:04 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,21 @@ const profileImageUrls = [
     "img/foto2.jpg",
     "img/foto3.jpg",
     "img/foto4.jpg",
-    "img/foto5.jpg"
+    "img/foto5.jpg",
+    "img/foto6.jpg",
+    "img/foto7.jpg",
+    "img/foto8.jpg",
+    "img/foto9.jpg",
+    "img/foto10.jpg",
+    "img/foto11.jpg",
+    "img/foto12.jpg",
+    "img/foto13.jpg",
+    "img/foto14.jpg",
+    "img/foto15.jpg",
+    "img/foto16.jpg",
+    "img/foto17.jpg",
+    "img/foto18.jpg",
+    "img/foto19.jpg",
+    "img/foto20.jpg"
     // Adicione os nomes exatos dos arquivos que estão na sua pasta /img/
 ];
